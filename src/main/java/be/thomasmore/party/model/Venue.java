0e7e46e0fdd30e9233e714216e30947c20379c2c
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-02-01 13:01:44 +0100

    chapter 2: thymeleaf each --> get allVenues from database

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -33,6 +33,14 @@ public class Venue {
         this.distanceFromPublicTransportInKm = distanceFromPublicTransportInKm;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getVenueName() {
         return venueName;
     }
