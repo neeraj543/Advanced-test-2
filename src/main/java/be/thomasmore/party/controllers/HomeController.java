ed0a970a697d46a7e40f42132a8d977bbad25086
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-09-11 15:04:55 +0200

    chapter 1: herhalings oefening

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.LocalDateTime.now;
+
 @Controller
 public class HomeController {
 
@@ -20,5 +25,14 @@ public class HomeController {
         model.addAttribute("myStreet", "Koning Albertlaan 23");
         model.addAttribute("myCity", "Leuven");
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        LocalDateTime now = now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        model.addAttribute("today", formatter.format(now));
+        model.addAttribute("payday", formatter.format(now.plusDays(30)));
+        return "pay";
     }
 }
