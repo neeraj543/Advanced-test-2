43e83d2bbb7109fd669fb8b017e04fcfd85ef854
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-09-11 17:48:56 +0200

    chapter 1: herhaling - fill db with 1 row

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ insert into VENUE (ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
                    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
 values (1, 'De Club', 'https://transitm.mechelen.be/de-club',
         150, false, true, false, true, 'Mechelen', 2);
+
+insert into CLIENT (ID, NAME, NR_OF_ORDERS, TOTAL_AMOUNT, DISCOUNT_TAKEN)
+values (1, 'Vera', '50', 5000, 100);
